100a470cd6318fdba76020d409f13747d78335b7
DaniilKhlyzov <dankhlyzov@gmail.com>
2023-11-17 02:36:54 +0500

    драйвер общий wait локальный

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -7,32 +7,24 @@ using SeleniumExtras.WaitHelpers;
 
 namespace ConsoleApp
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("user-data-dir=C:\\AcerGrey\\AppData\\Local\\Google\\Chrome\\User Data\\Profile2");
             IWebDriver driver = new ChromeDriver(options);
-
-            try
-            {
                 LoginPage loginPage = new LoginPage();
-                SharePage sharePage = new SharePage();
+                SharePage sharePage = new SharePage(driver);
 
                 //loginPage.Login(driver,"hifizov@yandex.ru", "3730133vv");
-                sharePage.AddMember(driver, "dankhlyzov@gmail.com",
+                sharePage.AddMember( "dankhlyzov@gmail.com",
                     "https://www.notion.so/6890e249352f423f8d560cfcc863e844");
                 Thread.Sleep(10000);
-                sharePage.Remove(driver, "dankhlyzov@gmail.com",
+                //sharePage.Remove(driver, "dankhlyzov@gmail.com",
+                   // "https://www.notion.so/6890e249352f423f8d560cfcc863e844");
+                sharePage.CanView( "dankhlyzov@gmail.com",
                     "https://www.notion.so/6890e249352f423f8d560cfcc863e844");
-            }
-            finally
-            {
-                // Закрытие веб-драйвера и освобождение ресурсов
-                driver.Quit();
-                driver.Dispose();
-            }
         }
     }
 
@@ -55,70 +47,78 @@ namespace ConsoleApp
 
     public class SharePage
     {
-        private WebDriverWait wait;
-
-        public void AddMember(IWebDriver driver, string member, string url)
+        private WebDriver Driver { get; }
+        public SharePage(IWebDriver driver)
         {
-            driver.Navigate().GoToUrl("https://www.notion.so");
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
-            ClickShare(driver);
-            InputMember(driver,member);
+            Driver = (WebDriver)driver;
         }
 
-        public void Remove(IWebDriver driver, string member, string url)
+
+        public void AddMember(string member, string url)
         {
-            driver.Navigate().GoToUrl(url);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
-            ClickShare(driver);
+            Driver.Navigate().GoToUrl(url);
+            ClickShare();
+            InputMember(member);
+        }
+
+        public void Remove(string member, string url)
+        {
+            Driver.Navigate().GoToUrl(url);
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
+            ClickShare();
             ClickMember(member);
             var remove = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[contains(text(),'Remove')]")));
             remove.Click();
+            
         }
-        public void CanEdit(IWebDriver driver, string member, string url)
+        public void CanEdit(string member, string url)
         {
-            driver.Navigate().GoToUrl(url);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
-            ClickShare(driver);
+            Driver.Navigate().GoToUrl(url);
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
+            ClickShare();
             ClickMember(member);
             var canEdit = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[contains(text(),'Can edit')]")));
             canEdit.Click();
         }
-        public void CanView(IWebDriver driver, string member, string url)
+        public void CanView( string member, string url)
         {
-            driver.Navigate().GoToUrl(url);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
-            ClickShare(driver);
+            Driver.Navigate().GoToUrl(url);
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
+            ClickShare();
             ClickMember(member);
             var canView = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[contains(text(),'Can view')]")));
             canView.Click();
         }
-        public void CanComment(IWebDriver driver, string member, string url)
+        public void CanComment(string member, string url)
         {
-            driver.Navigate().GoToUrl(url);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
-            ClickShare(driver);
+            Driver.Navigate().GoToUrl(url);
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
+            ClickShare();
             ClickMember(member);
             var canComment = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[contains(text(),'R')]")));
             canComment.Click();
         }
 
-        private void ClickShare(IWebDriver driver)
+        private void ClickShare()
         {
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
             var share = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[contains(text(), 'Share')]")));
             share.Click();
         }
 
-        private void InputMember(IWebDriver driver,string member)
+        private void InputMember(string member)
         {
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
             var addMember = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("#notion-app > div > div.notion-overlay-container.notion-default-overlay-container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(2) > div > div > div > div > div > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(1) > div.notion-scroller.vertical.horizontal > div > input[type=email]")));
             addMember.SendKeys(member);
-            var invite = driver.FindElement(By.XPath("//div[contains(text(), 'Invite')]"));
+            var invite = Driver.FindElement(By.XPath("//div[contains(text(), 'Invite')]"));
             invite.SendKeys(Keys.Enter);
             invite.Click();
         }
 
         private void ClickMember(string member)
         {
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
             var memberr = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[contains(text(),'" + member + "' )]")));
             memberr.Click();
         }
